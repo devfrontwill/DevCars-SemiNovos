9ae69a0548be89c74aa24c90d96a6a9be2a76aef
Wiliam Roza <devfrontwill@gmail.com>
2024-05-22 23:37:20 -0300

    Feat:Criado a pagina de Cadastro veiculos, feito toda a validação do formulario.

--- src/pages/dashboard/new/index.tsx
+++ src/pages/dashboard/new/index.tsx
@@ -1,7 +1,167 @@
+import { Container } from "../../../components/container";
+import { DashboardHeader } from "../../../components/painelHeader";
+import { Input } from "../../../components/input";
+import { FiUpload } from 'react-icons/fi';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod";
+
+
+const schema = z.object({
+    name: z.string().min(3, "O preenchimento deste campo é obrigatório ! "),
+    model: z.string().min(3, "O preenchimento deste campo é obrigatório ! "),
+    year: z.string().min(4, "O preenchimento deste campo é obrigatório ! ")
+        .refine((value) => /^(\d{4})$/.test(value), {
+            message: "O ano do veículo digitado é inválido! "
+        }),
+    km: z.string().min(3, "O preenchimento deste campo é obrigatório ! "),
+    price: z.string().min(3, "O preenchimento deste campo é obrigatório ! "),
+    city: z.string().min(3, "O preenchimento deste campo é obrigatório ! "),
+    whatsapp: z.string().min(1, "O preenchimento deste campo é obrigatório ! ")
+        .refine((value) => /^(\d{11,12})$/.test(value), {
+            message: "O número de telefone digitado é inválido."
+        }),
+    description: z.string().min(1,"O preenchimento deste campo é obrigatório ! "),
+})
+
+type FormData = z.infer<typeof schema>;
+
+
 export default function New() {
+    const { register, handleSubmit, formState: { errors }, reset } = useForm<FormData>({
+        resolver: zodResolver(schema),
+        mode: "onChange"
+    })
+
+    function onSubmit(data: FormData) {
+        console.log(data);
+    }
+
+
     return (
-        <div>
-            <h1>DevMotors Cadastrar Carro</h1>
-        </div>
+        <Container>
+            <DashboardHeader />
+
+            <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2">
+                <button className="border-2 w-48 rounded-lg flex items-center justify-center cursor-pointer border-gray-600 h-32 md:w-48">
+                    <div className="absolute cursor-pointer">
+                        <FiUpload size={30} color="#000" />
+                    </div>
+
+                    <div className="cursor-pointer">
+                        <input className="opacity-0 cursor-pointer" type="file" accept="image/*" />
+                    </div>
+                </button>
+            </div>
+
+            <div className="w-full bg-white p-3 rounded-lg flex flex-col sm:flex-row items-center gap-2 mt-2">
+                <form
+                    className="w-full"
+                    onClick={handleSubmit(onSubmit)}
+                >
+                    <div className="mb-3">
+                        <p className="mb-2 font-medium">Nome do veículo: </p>
+                        <Input
+                            type="text"
+                            register={register}
+                            name="name"
+                            error={errors.name?.message}
+                            placeholder="Ex: Jeep Renegade Semi Novo"
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <p className="mb-2 font-medium">Modelo do veículo: </p>
+                        <Input
+                            type="text"
+                            register={register}
+                            name="model"
+                            error={errors.model?.message}
+                            placeholder="Ex: 2.0 Flex Turbo Automático"
+                        />
+                    </div>
+
+                    <div className="flex w-full mb-3 flex-row items-center gap-4">
+                        <div className="w-full">
+                            <p className="mb-2 font-medium">Ano: </p>
+                            <Input
+                                type="text"
+                                register={register}
+                                name="year"
+                                error={errors.year?.message}
+                                placeholder="Ex: 2023"
+                            />
+                        </div>
+
+                        <div className="w-full">
+                            <p className="mb-2 font-medium">Kilometragem: </p>
+                            <Input
+                                type="text"
+                                register={register}
+                                name="km"
+                                error={errors.km?.message}
+                                placeholder="Ex: 171.333"
+                            />
+                        </div>
+
+                    </div>
+
+                    <div className="flex w-full mb-3 flex-row items-center gap-4">
+                        <div className="w-full">
+                            <p className="mb-2 font-medium">Telefone/Whatsapp: </p>
+                            <Input
+                                type="text"
+                                register={register}
+                                name="whatsapp"
+                                error={errors.whatsapp?.message}
+                                placeholder="Ex: 02198765432"
+                            />
+                        </div>
+
+                        <div className="w-full">
+                            <p className="mb-2 font-medium">Cidade: </p>
+                            <Input
+                                type="text"
+                                register={register}
+                                name="city"
+                                error={errors.city?.message}
+                                placeholder="Ex: Rio de Janeiro - RJ"
+                            />
+                        </div>
+
+                    </div>
+
+                    <div className="w-full">
+                        <p className="mb-2 font-medium">Preço: </p>
+                        <Input
+                            type="text"
+                            register={register}
+                            name="price"
+                            error={errors.price?.message}
+                            placeholder="Ex: 75.000"
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <p className="mb-2 font-medium">Descrição</p>
+                        <textarea
+                            className="border-2 w-full rounded-md h-24 px-2"
+                            {...register("description")}
+                            name="description"
+                            id="description"
+                            placeholder="Digite aqui a descrição do veículo..."
+                        />
+                        {errors.description && <p className="mb-1 text-red-500">{errors.description?.message}</p>}
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="w-full rounded-md bg-zinc-900 text-white font-medium h-10">
+                        Cadastrar
+                    </button>
+
+                </form>
+            </div>
+        </Container>
     )
 }
